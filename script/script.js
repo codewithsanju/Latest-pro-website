06e53bcfea48004a3a75f117458e13ffe8d7c78f
codewithsanju <sanjeevkumar919890@gmail.com>
2024-06-18 18:08:48 +0530

    commit

--- script/script.js
+++ script/script.js
@@ -1,1 +1,19 @@
- 
+//  navbar js
+//Javascript for Navigation effect on scroll
+window.addEventListener("scroll", function () {
+  var header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+//Javascript for responsive navigation sidebar Nav
+var menu = document.querySelector(".menu");
+var menuBtn = document.querySelector(".menu-btn");
+var closeBtn = document.querySelector(".close-btn");
+
+menuBtn.addEventListener("click", () => {
+  menu.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+  menu.classList.remove("active");
+});
