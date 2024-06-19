0fc55cdee31bf488a7a00a1ac8b5cdf2e31f86ec
codewithsanju <sanjeevkumar919890@gmail.com>
2024-06-19 18:56:21 +0530

    commit

--- script/script.js
+++ script/script.js
@@ -17,3 +17,33 @@ menuBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
   menu.classList.remove("active");
 });
+
+// why choose us
+const slider = document.querySelector(".slider");
+
+// This is script file
+
+$(".testimonials-container").owlCarousel({
+  loop: true,
+  autoplay: true,
+  autoplayTimeout: 6000,
+  margin: 10,
+  nav: true,
+  navText: [
+    "<i class='fa-solid fa-arrow-left'></i>",
+    "<i class='fa-solid fa-arrow-right'></i>",
+  ],
+  responsive: {
+    0: {
+      items: 1,
+      nav: false,
+    },
+    600: {
+      items: 1,
+      nav: true,
+    },
+    768: {
+      items: 2,
+    },
+  },
+});
