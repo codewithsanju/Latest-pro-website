2da68e209d517db6c469e8f9590eaad277a2e5bc
codewithsanju <sanjeevkumar919890@gmail.com>
2024-06-21 18:36:41 +0530

    commit

--- script/script.js
+++ script/script.js
@@ -1,11 +1,9 @@
-//  navbar js
-//Javascript for Navigation effect on scroll
 window.addEventListener("scroll", function () {
   var header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 0);
 });
 
-//Javascript for responsive navigation sidebar Nav
+// JavaScript for responsive navigation sidebar Nav
 var menu = document.querySelector(".menu");
 var menuBtn = document.querySelector(".menu-btn");
 var closeBtn = document.querySelector(".close-btn");
@@ -18,11 +16,19 @@ closeBtn.addEventListener("click", () => {
   menu.classList.remove("active");
 });
 
+// Close the menu when a link is clicked
+var menuLinks = document.querySelectorAll(".menu a");
+
+menuLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    menu.classList.remove("active");
+  });
+});
+
 // why choose us
 const slider = document.querySelector(".slider");
 
 // This is script file
-
 $(".testimonials-container").owlCarousel({
   loop: true,
   autoplay: true,
